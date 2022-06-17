c4988c081931a4e0853dd56e4fb4e6902a20640d
Vittorio Chiuselli <vittorio.chiuselli@gmail.com>
2022-06-17 11:42:43 +0200

    Creato timer per primo pulsante nello shop

---
+++ Assets/script/timer_reward.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class timer_reward : MonoBehaviour {
+   float msToWait;
+    Button ClickButton;
+    public ulong lastTimeClicked;
+
+    private void Start() {
+        msToWait = 10000f;
+        ClickButton = gameObject.GetComponent<Button>();
+
+
+        if (PlayerPrefs.HasKey("LastTimeClicked")) {
+            lastTimeClicked = ulong.Parse(PlayerPrefs.GetString("LastTimeClicked"));
+        }
+        else {
+            msToWait = 300f;
+            lastTimeClicked = (ulong)DateTime.Now.Ticks;
+            PlayerPrefs.SetString("LastTimeClicked", lastTimeClicked.ToString());
+        }
+
+        if (!Ready())
+            ClickButton.interactable = false;
+    }
+
+    private void Update() {
+        if (!ClickButton.IsInteractable()) {
+            if (Ready()) {
+                ClickButton.interactable = true;
+                return;
+            }
+        }
+    }
+
+
+    public void Click() {
+        lastTimeClicked = (ulong)DateTime.Now.Ticks;
+        PlayerPrefs.SetString("LastTimeClicked", lastTimeClicked.ToString());
+        ClickButton.interactable = false;
+    }
+
+    private bool Ready() {
+        ulong diff = ((ulong)DateTime.Now.Ticks - lastTimeClicked);
+        ulong m = diff / TimeSpan.TicksPerMillisecond;
+
+        float secondsLeft = (float)(msToWait - m) / 1000.0f;
+
+        if (secondsLeft < 0) {
+          //  ClickButton.interactable = false;
+            Debug.Log("qzazqz");
+            return true;
+        }
+
+        return false;
+    }
+
+    public void SetTimeToWait(float timeAmountInMs) {
+        msToWait = timeAmountInMs;
+    }
+
+}
+
+
+
